e7a3add74d26840fe09b73c442c4047e7b9c7547
Sanjee Yogeswaran <sanjee@MBP14.local>
2023-01-25 12:40:58 -0600

    reorganized seed

--- seed/s2/query.sql
+++
@@ -1,6 +0,0 @@
--- name: InsertBill :copyfrom
-INSERT INTO bills (billid, billnumber, billtype, introducedat, congress, summary, actions, sponsors, cosponsors, statusat, shorttitle, officialtitle
-    ) VALUES (
-    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12
-);
-
