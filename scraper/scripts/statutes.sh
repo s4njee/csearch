f1cb0405e57839ac6d7300044fd174fd1a5b7771
sanjee <sanjee.yogeswaran@gmail.com>
2024-09-25 22:01:11 -0500

    cleaned repo

--- scraper/scripts/statutes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-usc-run govinfo --collections=STATUTE --extract=mods,pdf
-usc-run statutes --volumes=65-86 --govtrack # bill status
-usc-run statutes --volumes=65-106 --textversions --extracttext # bill text
--- seed/env/bin/bills.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Refresh the bulk data collection.
-usc-run govinfo --bulkdata=BILLSTATUS
-
-# Turn into JSON and GovTrack-XML.
-usc-run bills --govtrack $@
